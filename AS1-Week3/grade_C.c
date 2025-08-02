70af1d909b7c7373b2565fac54e2d1d1fbb76eba
Dopyno <dopyno@yahoo.com>
2025-08-02 16:30:45 +0100

    week 3 _c

--- AS1-Week3/grade_C.c
+++ AS1-Week3/grade_C.c
@@ -1,20 +1,60 @@
 /*To achieve Marks (Between 50-59) (User Input and Study Goal Check)
-    Complete all previous steps, then:
+Complete all previous steps, then:
     • Retrieve study hours for Maths, Programming, and Networking from the user.
     • Add a weekly study goal and compare it with the total study hours.
     • Print whether the user has met the goal or needs to study more.*/
-    #include <stdio.h>
 
-    int main(){
+#include <stdio.h>
 
-        //* Create variable for three subjects and asign values directly within the code
-        int mathsHours = 6, programmingHours = 10, networkingHours = 4, totalHours;
+int main()
+{
+    // Create variable
+    int mathsHours, programmingHours, networkingHours, totalHours, goal;
 
-        //* Calculate the total hours
-        totalHours = mathsHours + programmingHours + networkingHours;
+    // Add a weekly study goal and compare it with the total study hours.
+    printf("\n\nPlease set a goal for learning this week: ");
+    scanf("%d", &goal);
 
-        //* Print the total into the terminal
-        printf("\nYou will need a total weekly study hours of %d, to pass all modules.\n\n", totalHours);
+    // Retrieve study hours for Maths, Programming, and Networking from the user.
+    printf("\nHow many hours did you study this week for Math: ");
+    scanf("%d", &mathsHours);
+    printf("How many hours did you study this week for Programming: ");
+    scanf("%d", &programmingHours);
+    printf("How many hours did you study this week for Networking: ");
+    scanf("%d", &networkingHours);
 
-        return 0;
+    // Calculate the total hours
+    totalHours = mathsHours + programmingHours + networkingHours;
+
+    // Print the goal and total into the terminal
+    printf("\nYor goal for this week was %d \nYou achieve a total of %d hours, as following: \n", goal, totalHours);
+    printf("%-12s %2d\n", "Maths:", mathsHours);
+    printf("%-12s %2d\n", "Programming:", programmingHours);
+    printf("%-12s %2d\n", "Networking:", networkingHours);
+
+    // Print whether the user has met the goal or needs to study more.
+    if (goal <= totalHours)
+    {
+        if (totalHours > goal)
+        {
+            printf("Amazing! You went beyond your study goal. Keep it up!");
+        }
+        else
+        {
+            printf("Great job! You have met your study goal.\n");
+        }
     }
+    else
+    {
+        if (goal - totalHours < 3)
+        {
+            printf(" You're almost there! Just a little more effort to reach your goal.");
+        }
+        else
+        {
+            printf("Keep going! You need to study more to meet your goal.\n");
+        }
+    }
+
+    return 0;
+}
