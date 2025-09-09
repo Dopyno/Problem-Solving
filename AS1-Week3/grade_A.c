2ccb8173f6a38e7ff78ba38f2e8bd024550a3717
Dopyno <dopyno@yahoo.com>
2025-09-09 21:01:07 +0100

    up

---
+++ AS1-Week3/grade_A.c
@@ -0,0 +1,8 @@
+// To achieve Marks (Between 70-100) (Dynamic Input and Enhanced Comparison)
+// Complete all previous steps, then:
+// • Allow the user to specify the number of subjects dynamically.
+// • Allow the user to type names of all subjects they want to track.
+// • Retrieve study hours for the chosen subjects for two weeks.
+// • Print a detailed breakdown of study hours for each week.
+// • Compare total study hours between weeks and identify increases, decreases,
+// or no changes in each subject.
