531649e266a88bd0716dd95e8e57af2ce4372807
Dopyno <dopyno@yahoo.com>
2025-09-10 20:51:00 +0100

    as3

---
+++ AS3-Week5/grade_D.c
@@ -0,0 +1,6 @@
+// 1. Write a program that converts a single day's steps into calories burned.
+// 2. Prompt the user to input the number of steps taken in a day.
+// 3. Use a fixed conversion rate (any number between 0.04 and 0.06 calories per step).
+// 4. Display the total calories burned.
+// 5. Provide screenshots of the code and its outputs.
+// 6. Use appropriate headings in the Word document to ensure the task is easily identifiable.
