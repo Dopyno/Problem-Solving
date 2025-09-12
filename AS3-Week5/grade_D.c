1c355b477dc821179df69b137cdb5eda30501224
Dopyno <dopyno@yahoo.com>
2025-09-12 19:00:57 +0100

    Week 3

--- AS3-Week5/grade_D.c
+++ AS3-Week5/grade_D.c
@@ -4,3 +4,18 @@
 // 4. Display the total calories burned.
 // 5. Provide screenshots of the code and its outputs.
 // 6. Use appropriate headings in the Word document to ensure the task is easily identifiable.
+
+#include<stdio.h>
+
+int main(){
+    int steps = 0;
+    float calories = 0, unitConversion = 0.05;
+    printf("\nPlease enter the number of steps from today: ");
+    scanf("%d", &steps);
+    calories = steps * unitConversion;
+
+    printf("Today you burn %.2f calories.\n", calories);
+
+
+    return 0;
+}
