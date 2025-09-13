9f618ac3a31fe0d06bca715b0737494a28de403e
Dopyno <dopyno@yahoo.com>
2025-09-13 09:10:48 +0100

    up

--- as4_Week6/grade_D.c
+++ as4_Week6/grade_D.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
 
 int main(){
-    char daysOfTheWeek[7] = {'S', 'M', 'T', 'W', 'T', 'F', 'S'};
-    enum months {"January", "February", "March", "April", "May", "June", "July", "August", /
-    "September", "October", "November", "December"};
-}
+    // declaring the variable
+    int monthNumber, startingWeekDay, fixedMonth = 30;
+    
+     char daysOfTheWeek[7] = {'S', 'M', 'T', 'W', 'T', 'F', 'S'};
+    enum months {January, February, March, April, May, June, July, August,
+    September, October, November, December};
+
+    printf("Please enter the month number you want to display (1 - 12): ");
+    scanf("%d", &monthNumber);
+    printf("Please enter the starting week day (1 = Monday, 7 = Sunday): ");
+    scanf("%d", &startingWeekDay);
+
+    //adjust for zero based index of the enums
+    enum months currentMonth = monthNumber - 1;
+
 
     return 0;
 }
