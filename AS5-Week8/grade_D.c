41f6ee871a20d844b5dcd48fd4befab3cf7d2c46
Dopyno <dopyno@yahoo.com>
2025-09-14 14:54:17 +0100

    as5

--- AS5-Week8/grade_D.c
+++ AS5-Week8/grade_D.c
@@ -1,40 +1,27 @@
-// Requirements:
-// • Prompt the user to enter the number of days
-// • Based on that number, create an array to store temperature readings for each day
-// • Allow the user to input temperatures one by one
-// • Implement a menu with the following options:
-
-// To achieve Marks (Between 40-49):
-// • Ask the user for how many days they wish to record temperature readings for.
-// • Allow user input to store all temperature readings in an array of appropriate size.
-// • Implement menu options 1 and 2 only.
-// • Demonstrate user using both options to display all entered temperatures and to
-// calculate the average.
-// • Submit screenshots of code and outputs.
-
 #include <stdio.h>
 
 int main()
 {
-    int numberOfDays, userOption;
-    
+    int numberOfDays, userOption, total = 0;
+    int averageTemp = 0.0;
+
     // ask the user for how many days want to create the record
     printf("\nPlease enter the number of days you want to record temperature readings: ");
     scanf("%d", &numberOfDays);
-    
     int tempReadings[numberOfDays]; // create an array with the size based on nr of days chosen by the user
 
-    printf("Please input the temperature readings: \n");
+    printf("Please input the temperature readings in °C: \n");
     // create a for loop to record the readings
     for (int i = 0; i < numberOfDays; i++)
     {
         printf("Please enter the value nr: %d\n", i + 1);
         scanf("%d", &tempReadings[i]);
+        total += tempReadings[i]; // capture the total to calculate the average
     }
     printf("All the readings stored successfully!\n\n");
 
     do
-    {
+    { // display the menu
         printf("1. Display all temperature readings.\n");
         printf("2. Calculate and display the average temperature overall.\n");
         printf("3. Find and display the highest and lowest temperature.\n");
@@ -44,14 +31,29 @@ int main()
         printf("7. Exit the program.\n");
         printf("Please chose an option: \n");
         scanf("%d", &userOption);
-        
-        switch (userOption)
+
+        switch (userOption) // use switch to access the menu 
         {
-            case 1:
-            /* code */
+        case 1:
+            printf("\nFull list of you recordings: \n"); // print a list of all recordings
+            for (int i = 0; i < numberOfDays; i++)
+            {
+                printf("The temperature for day %d: %d°C\n", i + 1, tempReadings[i]);
+            }
+            printf("\n");
             break;
-            case 7:
+        case 2:
+            averageTemp = total / numberOfDays; // calculate the average temp
+            printf("\nThe average temperature of %d days is: %d°C\n", numberOfDays, averageTemp);
+            printf("\n");
+            break;
+        case 7:
             printf("Good bye!\n");
+            printf("\n");
+            break;
+        default:
+            printf("Error: Invalid option, please try again!\n");
+            printf("\n");
             break;
         }
     } while (userOption != 7);
