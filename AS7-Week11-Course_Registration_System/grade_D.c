00b65f6c34793749a5bf409b3f929ffa1500e3d8
Dopyno <dopyno@yahoo.com>
2025-09-15 22:08:07 +0100

    update

--- AS7-Week11-Course_Registration_System/grade_D.c
+++ AS7-Week11-Course_Registration_System/grade_D.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
+//defining the struct with 3 elements id, course code and status
+typedef struct{ 
+    int id;
+    char courseCode[20];
+    char status[10];
+} Course;
+
+// create and write a file
+void saveToAFile(){
+    
+}
+
 int main(){
 
 
