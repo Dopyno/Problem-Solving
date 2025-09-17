3aa2dd29f51e0d68a3dc0e525c4a7e646e442bbc
Dopyno <dopyno@yahoo.com>
2025-09-17 23:09:52 +0100

    A

--- AS1-Week3-Study_Hours_Monitor/grade_A.c
+++ AS1-Week3-Study_Hours_Monitor/grade_A.c
@@ -1,9 +1,70 @@
-// To achieve Marks (Between 70-100) (Dynamic Input and Enhanced Comparison)
-// Complete all previous steps, then:
-// • Allow the user to specify the number of subjects dynamically.
-// • Allow the user to type names of all subjects they want to track.
-// • Retrieve study hours for the chosen subjects for two weeks.
-// • Print a detailed breakdown of study hours for each week.
-// • Compare total study hours between weeks and identify increases, decreases,
-// or no changes in each subject.
+#include <stdio.h>
 
+int main()
+{
+    int numberOfSubjects;
+
+    printf("\n===    Study Hours Monitor    ===\n\n");
+    // ask the user for the number of subjects to store
+    printf("Enter the number of subjects: ");
+    scanf("%d", &numberOfSubjects);
+    printf("\n");
+
+    char subjects[numberOfSubjects][25];
+    int week1Hours[numberOfSubjects];
+    int week2Hours[numberOfSubjects];
+    int goalWeek1, goalWeek2, totalWeek1 = 0, totalWeek2 = 0;
+
+
+
+    for (int i = 0; i < numberOfSubjects; i++)
+    {
+        printf("Enter the name of the subject %d: ", i + 1);
+        scanf(" %[^\n]", subjects[i]);
+    }
+    printf("\n");
+    printf("Enter your study goal for week 1: \n");
+    scanf("%d", &goalWeek1);
+    printf("Enter your study goal for week 2: \n");
+    scanf("%d", &goalWeek2);
+
+    printf("\nPlease enter the hours studied for week 1: \n");
+    for (int i = 0; i < numberOfSubjects; i++)
+    {
+        printf("Hours studied for %s: ", subjects[i]);
+        scanf("%d", &week1Hours[i]);
+        totalWeek1 += week1Hours[i];
+    }
+
+    printf("\nPlease enter the hours studied for week 2: \n");
+    for (int i = 0; i < numberOfSubjects; i++)
+    {
+        printf("Hours studied for %s: ", subjects[i]);
+        scanf("%d", &week2Hours[i]);
+        totalWeek2 += week2Hours[i];
+    }
+    printf("\n");
+    // print detail breakdown for week 1
+    printf("Week 1: Goal = %d, Total = %d\n", goalWeek1, totalWeek1);
+    for (int i = 0; i < numberOfSubjects; i++)
+    {
+        printf("%s : %d hours\n", subjects[i], week1Hours[i]);
+    }
+    printf("\n");
+    // print detail breakdown for week 1
+    printf("Week 1: Goal = %d, Total = %d\n", goalWeek2, totalWeek2);
+    for (int i = 0; i < numberOfSubjects; i++)
+    {
+        printf("%s : %d hours\n", subjects[i], week1Hours[i]);
+    }
+    printf("\n");
+    printf("Week 1 and Week 2 - comparison: \n");
+    if (totalWeek2 > totalWeek1)
+        printf("Increase in study hours, well done!\n");
+    else if (totalWeek2 < totalWeek1)
+        printf("Decrease in study hours, you need some improvement!\n");
+    else
+        printf("No change in study hours.\n");
+
+    return 0;
+}
